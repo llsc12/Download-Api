cfc92f0c7ac754871f62692fa6ba705da8481146
llsc12 <42747613+llsc12@users.noreply.github.com>
2021-08-01 23:05:38 +0100

    add poster attribute

---
+++ bot/commands/API/getInfo.js
@@ -0,0 +1,9 @@
+const discord = require("discord.js")
+module.exports = {
+  name:'getinfo',
+  aliases:['info', 'video', 'download', 'dl'],
+  description:'Get information, links and downloads of a video',
+  execute(client, message, args) {
+    
+  }
+}
