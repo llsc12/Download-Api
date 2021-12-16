bf3a32c6bf5e6ea617dda24d4847861de5489534
llsc12 <42747613+llsc12@users.noreply.github.com>
2021-12-16 17:50:22 +0000

    stuff

--- versions/1/thumbnailProxy.js
+++ versions/1/thumbnailProxy.js
@@ -1,34 +1,34 @@
-const fetch = require('node-fetch');
+const fetch = require('node-fetch'), ytdl = require('ytdl-core');
 //const axios = require('axios');
 module.exports = {
   endpoint:'thumbnails',
   async execute(req, res) {
     let imgpath = req.query.path
-    if (!imgpath) return res.send({error:'No path provided. Add path as query.'})
-    res.set('Content-Type', 'image/jpeg')
-    imgpath = `https://i.ytimg.com/${imgpath}`
+    let imgid = req.query.id
+    if (!imgpath && !imgid) return res.send({error:'No path provided. Add path as query.'})
 
-    //fetch image path and pipe it to res
-    try {
-      let response = await fetch(imgpath);
-      let image = await response.body;
-      image.pipe(res);
-    } catch (e) {
-      res.send({error: `Unknown error occurred`}) 
+    if (imgpath) {
+      imgpath = `https://i.ytimg.com/${imgpath}`
+      //fetch image path and pipe it to res
+      try {
+        let response = await fetch(imgpath);
+        let image = await response.body;
+        res.set('Content-Type', 'image/jpeg')
+        image.pipe(res);
+      } catch (e) {
+        res.send({error: `Unknown error occurred`}) 
+      }
+    } else if (imgid) {
+      //fetch image id and pipe it to res
+      try {
+        let vidinfo = await ytdl.getInfo(imgid)
+        let response = await fetch(vidinfo.videoDetails.thumbnails[vidinfo.videoDetails.thumbnails.length-1].url);
+        let image = await response.body;
+        res.set('Content-Type', 'image/jpeg')
+        image.pipe(res);
+      } catch (e) {
+        res.send({error: `Unknown error occurred`}) 
+      }
     }
-    /*
-    axios({
-      url: imgpath,
-      responseType: 'stream',
-    }).then(
-      response =>
-        new Promise((resolve, reject) => {
-          response.data
-            .pipe(res)
-            .on('finish', () => resolve())
-            .on('error', e => reject(e));
-        }),
-    );
-    */
   }
 }
--- versions/2/trending.js
+++ versions/2/trending.js
@@ -4,15 +4,14 @@ module.exports = {
   async execute(req, res) {
     const pages = ["default", "gaming", "music"]
     const cc = req.query.cc || "UK"
-    const page = req.query.page || "default"
+    let page = req.query.page || "default"
     if (!pages.includes(page)) {
-      const trending = await ytrend.trending(cc)
-      res.json(trending)
+      page = "default"
     }
     const params = {
       geoLocation: cc.toUpperCase(),
       parseCreatorOnRise: false,
-      page: 'music'
+      page: page
     }
     const trending = await ytrend.scrape_trending_page(params)
     let videos = []
