b4dcee62afdff5ccde9ceb911445c5b6ec47b20b
llsc12 <42747613+llsc12@users.noreply.github.com>
2021-12-26 21:00:22 +0000

    idk

--- versions/1/thumbnailProxy.js
+++ versions/1/thumbnailProxy.js
@@ -22,10 +22,12 @@ module.exports = {
       //fetch image id and pipe it to res
       try {
         let vidinfo = await ytdl.getInfo(imgid)
-        let response = await fetch(vidinfo.videoDetails.thumbnails[vidinfo.videoDetails.thumbnails.length-1].url);
-        let image = await response.body;
-        res.set('Content-Type', 'image/jpeg')
-        image.pipe(res);
+        let index = vidinfo.videoDetails.thumbnails.length -1
+        while (vidinfo.videoDetails.thumbnails[index].url.endsWith(".webp")) {
+          index=index-1
+        }
+        let vidimgurl = vidinfo.videoDetails.thumbnails[index].url
+        res.send(vidimgurl)
       } catch (e) {
         res.send({error: `Unknown error occurred`}) 
       }
